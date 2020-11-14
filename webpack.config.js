a853b53776f92303e3c03ac45a7af50108b6c073
Ed de Almeida <edvaldoajunior@gmail.com>
2020-11-14 13:40:35 -0300

    Webpack configurado

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    output: {
+        path: path.resolve(__dirname, 'public/javascripts'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ],
+            },
+        ],
+    },
+};
