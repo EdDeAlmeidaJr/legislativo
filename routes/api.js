6a2fca10f9f18aabd8abb5c1bb6b87fa3f374d31
Ed de Almeida <edvaldoajunior@gmail.com>
2020-11-19 11:31:26 -0300

    Testando o node-fetch

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,11 @@
-const { default: Axios } = require('axios');
+var fetch = require('node-fetch');
 var express = require('express');
 var router = express.Router();
 
+
 /* GET PECs */
 router.get('/deputados', async function(req, res, next) {
-  const pecs = await Axios.get('https://dadosabertos.camara.leg.br/api/v2/deputados?itens=100');
-  res.send(pecs);
+  res.send(await fetch('https://dadosabertos.camara.leg.br/api/v2/deputados?pagina=6&itens=100'));
 });
 
 module.exports = router;
